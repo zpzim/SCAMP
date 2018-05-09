abc6ad6bfb2b1848e39c9bfdbbfa9b764f788057
zpzim <zpzimmerman@gmail.com>
2018-05-09 16:50:28 -0700

    Added larger test input

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,11 +4,11 @@ EXECUTABLE=../src/SCRIMP-GPU
 ROOT_DIR_INPUT=SampleInput
 ROOT_DIR_OUTPUT=SampleOutput
 WINDOWSZ=(100)
-TILE_SZ=(1000000 8000 4000 2000 1000 500)
+TILE_SZ=(2000000 1000000 500000 250000 125000 62500 31250 15625 8000 4000)
 #TILE_SZ=(10000000)
-INPUT_FILES=(randomwalk8K randomwalk16K randomwalk32K randomwalk64K)
-NUM_TESTS=3
-NUM_TILE_SZ=5
+INPUT_FILES=(randomwalk8K randomwalk16K randomwalk32K randomwalk64K randomwalk1M)
+NUM_TESTS=4
+NUM_TILE_SZ=9
 
 for k in `seq 0 $NUM_TESTS`;
 do
@@ -21,12 +21,16 @@ do
         for i in `seq 0 $NUM_TILE_SZ`;
         do
             tile_sz=${TILE_SZ[i]}
-            echo "$EXECUTABLE $j $tile_sz $INPUT_FILE mp mpi 0"
-            `$EXECUTABLE $j $tile_sz $INPUT_FILE mp mpi 0`
-            X=`diff -U 0 mpi $COMPARE_MPI | grep ^@ | wc -l`
-            echo "$X flips"
-            echo ./difference.py mp $COMPARE_MP out
-            ./difference.py mp $COMPARE_MP out
+            count=`wc -l $INPUT_FILE | awk '{print $1}'`
+            echo $(($count * 2))
+            if [ $tile_sz -lt $(($count * 2)) ]; then
+                echo "$EXECUTABLE $j $tile_sz $INPUT_FILE mp mpi 0"
+                `$EXECUTABLE $j $tile_sz $INPUT_FILE mp mpi 0`
+                X=`diff -U 0 mpi $COMPARE_MPI | grep ^@ | wc -l`
+                echo "$X flips"
+                echo ./difference.py mp $COMPARE_MP out
+                ./difference.py mp $COMPARE_MP out
+            fi
         done
     done
 done
