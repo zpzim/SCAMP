64967802dfae09ffb28453bb8a4b3eba54afbb02
zpzim <zpzimmerman@gmail.com>
2019-01-09 16:53:50 -0800

    Added missing cases in cufft error printing

--- src/fft_helper.h
+++ src/fft_helper.h
@@ -10,36 +10,39 @@ static const char *_cudaGetErrorEnum(cufftResult error) {
   switch (error) {
     case CUFFT_SUCCESS:
       return "CUFFT_SUCCESS";
-
     case CUFFT_INVALID_PLAN:
       return "CUFFT_INVALID_PLAN";
-
     case CUFFT_ALLOC_FAILED:
       return "CUFFT_ALLOC_FAILED";
-
     case CUFFT_INVALID_TYPE:
       return "CUFFT_INVALID_TYPE";
-
     case CUFFT_INVALID_VALUE:
       return "CUFFT_INVALID_VALUE";
-
     case CUFFT_INTERNAL_ERROR:
       return "CUFFT_INTERNAL_ERROR";
-
     case CUFFT_EXEC_FAILED:
       return "CUFFT_EXEC_FAILED";
-
     case CUFFT_SETUP_FAILED:
       return "CUFFT_SETUP_FAILED";
-
     case CUFFT_INVALID_SIZE:
       return "CUFFT_INVALID_SIZE";
-
+    case CUFFT_INVALID_DEVICE:
+      return "CUFFT_INVALID_DEVICE";
     case CUFFT_UNALIGNED_DATA:
       return "CUFFT_UNALIGNED_DATA";
+    case CUFFT_NO_WORKSPACE:
+      return "CUFFT_NO_WORKSPACE";
+    case CUFFT_NOT_IMPLEMENTED:
+      return "CUFFT_NOT_IMPLEMENTED";
+    case CUFFT_PARSE_ERROR:
+      return "CUFFT_PARSE_ERROR";
+    case CUFFT_LICENSE_ERROR:
+      return "CUFFT_LICENSE_ERROR";
+    case CUFFT_NOT_SUPPORTED:
+      return "CUFFT_NOT_SUPPORTED";
+    case CUFFT_INCOMPLETE_PARAMETER_LIST:
+      return "CUFFT_INCOMPLETE_PARAMETER_LIST";
   }
-
-  return "<unknown>";
 }
 #endif
 
