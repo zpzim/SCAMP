75589a7a8ad78d77b1182603dc747d8a23482cc4
zpzim <zpzimmerman@gmail.com>
2019-06-27 09:37:35 -0700

    Fixed GPU memory leak introduced by previous pull request

--- src/qt_helper.cpp
+++ src/qt_helper.cpp
@@ -77,51 +77,43 @@ SCAMPError_t qt_compute_helper::compute_QT(double *QT, const double *T,
 
   const int n = size - window_size + 1;
 
-  try {
-    init();
-    CHECK_CUFFT_ERRORS(cufftSetStream(fft_plan, s))
+  CHECK_CUFFT_ERRORS(cufftSetStream(fft_plan, s))
 
-    CHECK_CUFFT_ERRORS(cufftSetStream(ifft_plan, s))
-    // Compute the FFT of the time series
-    // For some reason the input parameter to cufftExecD2Z is not held const
-    // by cufft I see nowhere in the documentation that the input vector is
-    // modified using const_cast as a hack to get around this...
-    CHECK_CUFFT_ERRORS(
-        cufftExecD2Z(fft_plan, const_cast<double *>(T), Tc))  // NOLINT
+  CHECK_CUFFT_ERRORS(cufftSetStream(ifft_plan, s))
+  // Compute the FFT of the time series
+  // For some reason the input parameter to cufftExecD2Z is not held const
+  // by cufft I see nowhere in the documentation that the input vector is
+  // modified using const_cast as a hack to get around this...
+  CHECK_CUFFT_ERRORS(
+      cufftExecD2Z(fft_plan, const_cast<double *>(T), Tc))  // NOLINT
 
-    // Reverse and zero pad the query
-    launch_populate_reverse_pad(Q, Q_reverse_pad, qmeans, window_size, size,
-                                fft_work_size, s);
-    error = cudaPeekAtLastError();
-    if (error != cudaSuccess) {
-      free();
-      return SCAMP_CUDA_ERROR;
-    }
-
-    CHECK_CUFFT_ERRORS(cufftExecD2Z(fft_plan, Q_reverse_pad, Qc))
-
-    launch_elementwise_multiply_inplace(Tc, Qc, cufft_data_size, fft_work_size,
-                                        s);
-    error = cudaPeekAtLastError();
-    if (error != cudaSuccess) {
-      free();
-      return SCAMP_CUDA_ERROR;
-    }
-
-    CHECK_CUFFT_ERRORS(cufftExecZ2D(ifft_plan, Qc, Q_reverse_pad))
-
-    launch_normalized_aligned_dot_products(Q_reverse_pad, size, window_size, n,
-                                           QT, fft_work_size, s);
-    error = cudaPeekAtLastError();
-
-    if (error != cudaSuccess) {
-      free();
-      return SCAMP_CUDA_ERROR;
-    }
-  } catch (SCAMPException &e) {
-    free();
-    throw;
+  // Reverse and zero pad the query
+  launch_populate_reverse_pad(Q, Q_reverse_pad, qmeans, window_size, size,
+                              fft_work_size, s);
+  error = cudaPeekAtLastError();
+  if (error != cudaSuccess) {
+    return SCAMP_CUDA_ERROR;
   }
+
+  CHECK_CUFFT_ERRORS(cufftExecD2Z(fft_plan, Q_reverse_pad, Qc))
+
+  launch_elementwise_multiply_inplace(Tc, Qc, cufft_data_size, fft_work_size,
+                                      s);
+  error = cudaPeekAtLastError();
+  if (error != cudaSuccess) {
+    return SCAMP_CUDA_ERROR;
+  }
+
+  CHECK_CUFFT_ERRORS(cufftExecZ2D(ifft_plan, Qc, Q_reverse_pad))
+
+  launch_normalized_aligned_dot_products(Q_reverse_pad, size, window_size, n,
+                                         QT, fft_work_size, s);
+  error = cudaPeekAtLastError();
+
+  if (error != cudaSuccess) {
+    return SCAMP_CUDA_ERROR;
+  }
+
   return SCAMP_NO_ERROR;
 }
 #endif
