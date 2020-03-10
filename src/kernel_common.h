978a58f7c4ee0a5d6346e0697177c7eb3a6424a0
Dmitry Nikolaenko <parnumeric@gmail.com>
2020-03-11 00:42:20 +0100

    Update SCAMPKernelInputArgs for Windows platform

--- src/kernel_common.h
+++ src/kernel_common.h
@@ -9,14 +9,14 @@ static constexpr int NUM_EXTRA_OPERANDS = 3;
 template <typename T>
 struct SCAMPKernelInputArgs {
   SCAMPKernelInputArgs(Tile *t, bool transpose, bool ab_join);
-  T *__restrict__ cov;
-  const T *__restrict__ dfa;
-  const T *__restrict__ dfb;
-  const T *__restrict__ dga;
-  const T *__restrict__ dgb;
-  const T *__restrict__ normsa;
-  const T *__restrict__ normsb;
-  const T *__restrict__ extras[NUM_EXTRA_OPERANDS];
+  T *__restrict cov;
+  const T *__restrict dfa;
+  const T *__restrict dfb;
+  const T *__restrict dga;
+  const T *__restrict dgb;
+  const T *__restrict normsa;
+  const T *__restrict normsb;
+  const T *__restrict extras[NUM_EXTRA_OPERANDS];
   unsigned long long int *profile_a_length;
   unsigned long long int *profile_b_length;
   int64_t max_matches_per_tile;
