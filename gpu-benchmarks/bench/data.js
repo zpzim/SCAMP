b7421a31b73768a8296b7564c09e92ff8bc778fe
github-action-benchmark <github@users.noreply.github.com>
2022-02-13 12:33:09 -0800

    add Benchmark (googlecpp) benchmark result for 98b089b14c3f51de0a729419e586d52c04c716b9

--- gpu-benchmarks/bench/data.js
+++ gpu-benchmarks/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644780150793,
+  "lastUpdate": 1644784389885,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 5770451938.267797,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 62616296.999999985 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "98b089b14c3f51de0a729419e586d52c04c716b9",
+          "message": "Add various improvements to compile options. Including Automatic AVX detection for MSVC. (#91)\n\n* Add various improvements to compile options. Adds automatic AVX detection for MSVC.",
+          "timestamp": "2022-02-13T11:40:41-08:00",
+          "tree_id": "7fa6d66af821da480935020a421cf503470dc817",
+          "url": "https://github.com/zpzim/SCAMP/commit/98b089b14c3f51de0a729419e586d52c04c716b9"
+        },
+        "date": 1644784377845,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/-1/1048576",
+            "value": 3433724745.1767325,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 74037310 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/-1/1048576",
+            "value": 2943486778.996885,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 65017337.00000001 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/-1/1048576",
+            "value": 7931927475.14695,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 67874505.00000003 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/-1/1048576",
+            "value": 5774162684.101611,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 64769753.999999955 ns\nthreads: 1"
           }
         ]
       }
