3583ea62b0d5e856e4c3bc7ff0d0ed2e15a5e2ce
github-action-benchmark <github@users.noreply.github.com>
2022-02-13 23:05:05 +0000

    add Benchmark (googlecpp) benchmark result for 3e7d3b8cb92cd7c09ac2233e46dc0fcd2e9ef5ac

--- cpu-benchmarks/ubuntu-latest/g++/bench/data.js
+++ cpu-benchmarks/ubuntu-latest/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644783195415,
+  "lastUpdate": 1644793505942,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -1548,6 +1548,54 @@ window.BENCHMARK_DATA = {
             "value": 83124511855.00003,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 14063600.000000002 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e7d3b8cb92cd7c09ac2233e46dc0fcd2e9ef5ac",
+          "message": "Remove 'fast' reduction path for CPU kernels and go with the Eigen path. (#92)\n\nIt appears to almost always be faster.",
+          "timestamp": "2022-02-13T15:00:45-08:00",
+          "tree_id": "696158b8b6b58f0fb83e433729609c07c054397c",
+          "url": "https://github.com/zpzim/SCAMP/commit/3e7d3b8cb92cd7c09ac2233e46dc0fcd2e9ef5ac"
+        },
+        "date": 1644793505172,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/131072",
+            "value": 24110510121.000004,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 13824700.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/131072",
+            "value": 5590754157.000049,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 11962699.999999996 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/131072",
+            "value": 23039199108.00005,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 12348399.999999996 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/131072",
+            "value": 81565101169.99998,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 12188400.000000002 ns\nthreads: 1"
           }
         ]
       }
