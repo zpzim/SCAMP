27f3220fb397291d7932c45c276874b0ad81097e
github-action-benchmark <github@users.noreply.github.com>
2022-02-13 19:27:03 +0000

    add Benchmark (googlecpp) benchmark result for a09aed826cc4867f4754138678270bb8475afedd

--- cpu-benchmarks/macos-latest/g++/bench/data.js
+++ cpu-benchmarks/macos-latest/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644722116361,
+  "lastUpdate": 1644780423030,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -1452,6 +1452,54 @@ window.BENCHMARK_DATA = {
             "value": 13828297423.000095,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 14504000.000000073 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a09aed826cc4867f4754138678270bb8475afedd",
+          "message": "Refactor benchmarks suite, add gpu benchmarks. (#88)\n\n* Refactor Benchmark library and add GPU benchmarks\r\n\r\n* Reduce the number of CPU benchmarks computed.",
+          "timestamp": "2022-02-13T11:17:05-08:00",
+          "tree_id": "7059afba734693fece8c8570a08d8b1c0aced0f4",
+          "url": "https://github.com/zpzim/SCAMP/commit/a09aed826cc4867f4754138678270bb8475afedd"
+        },
+        "date": 1644780422087,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/131072",
+            "value": 15344175792.000101,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 13297999.999999998 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/131072",
+            "value": 9624809576.000074,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 6787999.999999999 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/131072",
+            "value": 17967782439.99995,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 7117999.999999996 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/131072",
+            "value": 41992694886.99997,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 6525000.000000003 ns\nthreads: 1"
           }
         ]
       }
