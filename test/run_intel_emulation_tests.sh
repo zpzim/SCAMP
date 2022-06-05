30d2373dfd93dedb8b96a3e3c25572c2d73ca70c
zpzim <zpzimmerman@gmail.com>
2022-06-05 09:30:35 -0700

    Add more coverage in emulation test for each profile type.

--- test/run_intel_emulation_tests.sh
+++ test/run_intel_emulation_tests.sh
@@ -4,17 +4,23 @@ SCAMP_EXECUTABLE=$1
 SDE_EXECUTABLE=$2
 
 ARCHS=("-snb -ivb -hsw -bdw -slt -slm -glm -glp -tnt -snr -skl -cnl -icl -skx -clx -cpx -icx -knl -knm -tgl -adl -spr") 
+PROFILES=("1NN_INDEX 1NN SUM_THRESH MATRIX_SUMMARY")
 PASSED=""
 FAILED=""
 for ARCH in $ARCHS;
 do
-  CMD="${SDE_EXECUTABLE} $ARCH -- ${SCAMP_EXECUTABLE} --window=100 --input_a_file_name=../test/SampleInput/randomwalk8K.txt --no_gpu --num_cpu_workers=1 --print_debug_info"
-  echo $CMD
-  if $CMD ; then
-    PASSED="${PASSED} $ARCH"
-  else
-    FAILED="${FAILED} $ARCH"
-  fi
+  for PROFILE in $PROFILES;
+  do
+    CMD="${SDE_EXECUTABLE} $ARCH -- ${SCAMP_EXECUTABLE} --window=100 --input_a_file_name=../test/SampleInput/randomwalk8K.txt --profile_type=$PROFILE --no_gpu --num_cpu_workers=1 --print_debug_info"
+    echo $CMD
+    if $CMD ; then
+      echo "PASS"
+      PASSED="${PASSED} $PROFILE/$ARCH"
+    else
+      echo "FAIL"
+      FAILED="${FAILED} $PROFILE/$ARCH"
+    fi
+  done
 done
 
 if [ $FAILED ]; then
