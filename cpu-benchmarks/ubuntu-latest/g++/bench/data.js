fd6e3ee87ab0c9a5b015be29e5774b60e31e72b2
github-action-benchmark <github@users.noreply.github.com>
2022-06-07 07:23:15 +0000

    add Benchmark (googlecpp) benchmark result for 4cc2208c7f620b796f83c81d5a8cb62e22e2c277

--- cpu-benchmarks/ubuntu-latest/g++/bench/data.js
+++ cpu-benchmarks/ubuntu-latest/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654554089195,
+  "lastUpdate": 1654586595467,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -1836,6 +1836,54 @@ window.BENCHMARK_DATA = {
             "value": 60598600674.00007,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 13973399.999999996 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4cc2208c7f620b796f83c81d5a8cb62e22e2c277",
+          "message": "Fix some build issues with redistributable binaries on windows and mac (#102)\n\n* Disable clang-tidy when building by default for now.\r\n\r\nThere are some issues with newer versions of clang-tidy which can cause broken builds.\r\n\r\n* Only depend on cpu_features when building a redistributable binary.\r\n\r\nMove Eigen dependency into the cpu_kernels module.\r\n\r\n* Add message during configuration when clang-tidy isn't enabled.\r\n\r\n* Add Action to publish dev packages to test pypi.",
+          "timestamp": "2022-06-07T00:05:03-07:00",
+          "tree_id": "b17c0d4ab66ca4954c3e83e058cc110e0aa5729a",
+          "url": "https://github.com/zpzim/SCAMP/commit/4cc2208c7f620b796f83c81d5a8cb62e22e2c277"
+        },
+        "date": 1654586594317,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/131072",
+            "value": 16067290474.000004,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 13140800.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/131072",
+            "value": 9307958078.000013,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 11285300.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/131072",
+            "value": 25914374474.99999,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 11367000.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/131072",
+            "value": 61024550948.000015,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 11193200 ns\nthreads: 1"
           }
         ]
       }
