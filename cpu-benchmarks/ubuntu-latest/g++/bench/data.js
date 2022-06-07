4ed09570d71aa1cdeaf04674fbc16b7cf6fc645c
github-action-benchmark <github@users.noreply.github.com>
2022-06-07 23:54:57 +0000

    add Benchmark (googlecpp) benchmark result for 47ab9235124f2d03961064473505491af513a8dd

--- cpu-benchmarks/ubuntu-latest/g++/bench/data.js
+++ cpu-benchmarks/ubuntu-latest/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654586595467,
+  "lastUpdate": 1654646097216,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -1884,6 +1884,54 @@ window.BENCHMARK_DATA = {
             "value": 61024550948.000015,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 11193200 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "47ab9235124f2d03961064473505491af513a8dd",
+          "message": "Clean up python module (#103)\n\n* Enable publishing intermetiate versions of SCAMP between releases to test pypi.\r\n\r\n* Only build the SCAMP executable if we aren't building the python module.\r\n\r\n* Only include relevant source files in pyscamp.\r\n\r\n* Bump pybind11 to v2.9.2\r\n\r\n* Add verbose messages indicating how cmake is invoked by pyscamp's setup.py\r\n\r\n* Fix failure detection in architecture emulation test.\r\n\r\n* Reduce warning spam from MSVC. Stop using -Wall on MSVC use W4 instead.\r\n\r\n* Fix pyscamp build issues that occur when CMAKE_BUILD_TYPE is set incorrectly and we aren't using a multi-config generator. Should fix some issues with using Ninja.\r\n\r\n* Allow a custom Python executable path to be provided when building pyscamp.",
+          "timestamp": "2022-06-07T16:51:15-07:00",
+          "tree_id": "09aef9b0ef24eb0bd5600b39cc6068cb4dce7df5",
+          "url": "https://github.com/zpzim/SCAMP/commit/47ab9235124f2d03961064473505491af513a8dd"
+        },
+        "date": 1654646096221,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/131072",
+            "value": 12738867675.999985,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 14202200.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/131072",
+            "value": 5598424674,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 12264400.000000002 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/131072",
+            "value": 22060383833.000004,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 12352400.000000006 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/131072",
+            "value": 50218016030,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 12527100 ns\nthreads: 1"
           }
         ]
       }
