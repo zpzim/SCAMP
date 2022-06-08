ba039e51ffed0c62b14171cb96ede7a1d568eec5
github-action-benchmark <github@users.noreply.github.com>
2022-06-08 06:12:41 +0000

    add Benchmark (googlecpp) benchmark result for f6e4f20d1781534c80df4e6bcf0faa19d02b53aa

--- cpu-benchmarks/windows-latest/cl/bench/data.js
+++ cpu-benchmarks/windows-latest/cl/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654646871013,
+  "lastUpdate": 1654668760903,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -1668,6 +1668,54 @@ window.BENCHMARK_DATA = {
             "value": 52987094899.99988,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 0 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "zpzim",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "zpzim",
+            "username": "zpzim"
+          },
+          "distinct": true,
+          "id": "f6e4f20d1781534c80df4e6bcf0faa19d02b53aa",
+          "message": "Sickbay windows hardware emulation test for now.",
+          "timestamp": "2022-06-07T22:58:16-07:00",
+          "tree_id": "fda452409f2b59289e9d32316a3395f83fe6e7a8",
+          "url": "https://github.com/zpzim/SCAMP/commit/f6e4f20d1781534c80df4e6bcf0faa19d02b53aa"
+        },
+        "date": 1654668757971,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/131072",
+            "value": 32253662800.00003,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 15625000 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/131072",
+            "value": 26717261299.99979,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 31250000 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/131072",
+            "value": 29004119100.000027,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 15625000 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/131072",
+            "value": 66213573900.00003,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 15625000 ns\nthreads: 1"
           }
         ]
       }
