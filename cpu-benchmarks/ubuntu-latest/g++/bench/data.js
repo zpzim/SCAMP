04efeac3928886c264ab08832f13d57dab8e2df7
github-action-benchmark <github@users.noreply.github.com>
2022-06-18 17:55:35 +0000

    add Benchmark (googlecpp) benchmark result for a16f33c498807b56e0105d5fc23344aaad676015

---
+++ cpu-benchmarks/ubuntu-latest/g++/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1655574935419,
+  "repoUrl": "https://github.com/zpzim/SCAMP",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a16f33c498807b56e0105d5fc23344aaad676015",
+          "message": "Improve benchmarking to add stable CPU benchmarks and reduce variance on Github runners. (#118)\n\n* Add a stable version of gcc/clang benchmarks on self-hosted linux box.\r\n\r\n* Update benchmarking to run smaller benchmarks. Output timing information in seconds.\r\n\r\n* Update docs to point to stable benchmark suites.",
+          "timestamp": "2022-06-18T10:52:55-07:00",
+          "tree_id": "aa3c204e0fb719b7016d86a95d7232bca453e9a2",
+          "url": "https://github.com/zpzim/SCAMP/commit/a16f33c498807b56e0105d5fc23344aaad676015"
+        },
+        "date": 1655574934243,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/32768",
+            "value": 0.8256150896999997,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.003291050000000001 s\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/32768",
+            "value": 0.3574636677000001,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0033597800000000006 s\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/32768",
+            "value": 1.6512662046999993,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0033451500000000007 s\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/32768",
+            "value": 3.084908918999986,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 0.0029708999999999985 s\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
