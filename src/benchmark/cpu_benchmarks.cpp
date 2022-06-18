018d052dc145359b551017027b5d600b2c23f92b
zpzim <zpzimmerman@gmail.com>
2022-06-17 17:31:15 -0700

    Update benchmarking to run smaller benchmarks. Output timing information in seconds.

--- src/benchmark/cpu_benchmarks.cpp
+++ src/benchmark/cpu_benchmarks.cpp
@@ -2,12 +2,12 @@
 #include "benchmark/common.h"
 
 static void benchmarkArgsCI(benchmark::internal::Benchmark* b) {
-  b->Args({1, 1 << 17});
+  b->Args({1, 1 << 15});
 }
 
-BENCHMARK(BM_1NN_INDEX_SELF_JOIN)->Apply(benchmarkArgsCI);
-BENCHMARK(BM_1NN_SELF_JOIN)->Apply(benchmarkArgsCI);
-BENCHMARK(BM_SUM_SELF_JOIN)->Apply(benchmarkArgsCI);
-BENCHMARK(BM_MATRIX_SELF_JOIN)->Apply(benchmarkArgsCI);
+BENCHMARK(BM_1NN_INDEX_SELF_JOIN)->Unit(benchmark::kSecond)->Apply(benchmarkArgsCI);
+BENCHMARK(BM_1NN_SELF_JOIN)->Unit(benchmark::kSecond)->Apply(benchmarkArgsCI);
+BENCHMARK(BM_SUM_SELF_JOIN)->Unit(benchmark::kSecond)->Apply(benchmarkArgsCI);
+BENCHMARK(BM_MATRIX_SELF_JOIN)->Unit(benchmark::kSecond)->Apply(benchmarkArgsCI);
 
 BENCHMARK_MAIN();
