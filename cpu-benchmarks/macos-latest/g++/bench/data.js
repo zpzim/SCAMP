e879e8a0c400f29a5234a75ce0b68790aa594b89
github-action-benchmark <github@users.noreply.github.com>
2022-06-18 21:05:44 +0000

    add Benchmark (googlecpp) benchmark result for ba40129d7615c06a2cc186b720e25183f4b5c20a

--- cpu-benchmarks/macos-latest/g++/bench/data.js
+++ cpu-benchmarks/macos-latest/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655575098914,
+  "lastUpdate": 1655586344069,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 2.6912426320000122,
             "unit": "s/iter",
             "extra": "iterations: 1\ncpu: 0.002023000000000011 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ba40129d7615c06a2cc186b720e25183f4b5c20a",
+          "message": "Add GPU integration tests. (#81)\n\nAdds GPU integration tests to verify output correctness of GPU kernels.",
+          "timestamp": "2022-06-18T13:37:28-07:00",
+          "tree_id": "4f69ab291f6b937e5b36b8aae3bb2c4ae203ed4f",
+          "url": "https://github.com/zpzim/SCAMP/commit/ba40129d7615c06a2cc186b720e25183f4b5c20a"
+        },
+        "date": 1655586343083,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/32768",
+            "value": 0.9397992732000603,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0018926 s\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/32768",
+            "value": 0.574171920200024,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0021248 s\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/32768",
+            "value": 1.1000136890999783,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.001897 s\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/32768",
+            "value": 2.574480117999883,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 0.0019599999999999895 s\nthreads: 1"
           }
         ]
       }
