29f96258cc21ecdcedf771fccfa69599ec782594
github-action-benchmark <github@users.noreply.github.com>
2022-08-04 14:59:01 +0000

    add Benchmark (googlecpp) benchmark result for 27e617febc69c476408ae05b217be395cc72aa35

--- cpu-benchmarks/windows-latest/clang-cl/bench/data.js
+++ cpu-benchmarks/windows-latest/clang-cl/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655586180827,
+  "lastUpdate": 1659625141462,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -94,6 +94,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BM_MATRIX_SELF_JOIN/1/32768",
             "value": 2.534464099999923,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 0 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "27e617febc69c476408ae05b217be395cc72aa35",
+          "message": "Fix some broken links in intro.rst (#119)",
+          "timestamp": "2022-08-04T07:51:56-07:00",
+          "tree_id": "dd44769e8b2614928cd8d09a124fe81845fdc641",
+          "url": "https://github.com/zpzim/SCAMP/commit/27e617febc69c476408ae05b217be395cc72aa35"
+        },
+        "date": 1659625138497,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/32768",
+            "value": 1.6017369700000017,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.00625 s\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/32768",
+            "value": 0.5252871599999935,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0046875 s\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/32768",
+            "value": 1.3120077199999969,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.003125 s\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/32768",
+            "value": 2.8202148999998826,
             "unit": "s/iter",
             "extra": "iterations: 1\ncpu: 0 s\nthreads: 1"
           }
