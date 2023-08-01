dbae0b2230829929ad6018b44a1cb88a759aa77d
github-action-benchmark <github@users.noreply.github.com>
2023-08-01 00:26:59 +0000

    add Benchmark (googlecpp) benchmark result for fe867f9cf755d149f1a7aa98b6ed0509c1311fac

--- cpu-benchmarks/windows-latest/clang-cl/bench/data.js
+++ cpu-benchmarks/windows-latest/clang-cl/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659625141462,
+  "lastUpdate": 1690849618830,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -144,6 +144,54 @@ window.BENCHMARK_DATA = {
             "value": 2.8202148999998826,
             "unit": "s/iter",
             "extra": "iterations: 1\ncpu: 0 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe867f9cf755d149f1a7aa98b6ed0509c1311fac",
+          "message": "Update README.md to include DIO from zenodo",
+          "timestamp": "2023-07-31T17:14:19-07:00",
+          "tree_id": "d2988c8498c94c5b3c4019ae370c118301eb5783",
+          "url": "https://github.com/zpzim/SCAMP/commit/fe867f9cf755d149f1a7aa98b6ed0509c1311fac"
+        },
+        "date": 1690849615009,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/32768",
+            "value": 1.5870904699999984,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0 s\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/32768",
+            "value": 0.31839153999999326,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.00625 s\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/32768",
+            "value": 1.1822938600000044,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.003125 s\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/32768",
+            "value": 2.47387144999999,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.00625 s\nthreads: 1"
           }
         ]
       }
