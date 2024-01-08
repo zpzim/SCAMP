2740ac7581b5af23082f42d7fb09e6aa24ffcdcf
github-action-benchmark <github@users.noreply.github.com>
2024-01-08 09:10:12 -0800

    add Benchmark (googlecpp) benchmark result for 3f3078e9abdfd4141fe19ce68500d9ab908353ba

--- cpu-benchmarks/linux-stable/g++/bench/data.js
+++ cpu-benchmarks/linux-stable/g++/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659625521401,
+  "lastUpdate": 1704733812343,
   "repoUrl": "https://github.com/zpzim/SCAMP",
   "entries": {
     "Benchmark": [
@@ -144,6 +144,54 @@ window.BENCHMARK_DATA = {
             "value": 1.7182085000909866,
             "unit": "s/iter",
             "extra": "iterations: 10\ncpu: 0.0017090126999999996 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zpzimmerman@gmail.com",
+            "name": "Zach Zimmerman",
+            "username": "zpzim"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f3078e9abdfd4141fe19ce68500d9ab908353ba",
+          "message": "Add support for cuda 12 builds (#124)\n\n* Adds support for builing for compute capabilities 87, 89, and 90. \r\n\r\n* Fixes issues with CUDA_ARCHITECTURES not being set correctly based on cuda compiler version.\r\n\r\n* Fix some broken test scripts.\r\n\r\n* Bump SDE version for arch emulation test",
+          "timestamp": "2024-01-08T00:35:29-08:00",
+          "tree_id": "4bf8750b26a17e93ab412acda21fe81cf34f943c",
+          "url": "https://github.com/zpzim/SCAMP/commit/3f3078e9abdfd4141fe19ce68500d9ab908353ba"
+        },
+        "date": 1704733801221,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_1NN_INDEX_SELF_JOIN/1/32768",
+            "value": 0.6697879581995949,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0016829839 s\nthreads: 1"
+          },
+          {
+            "name": "BM_1NN_SELF_JOIN/1/32768",
+            "value": 0.29196930870020876,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0016270017000000006 s\nthreads: 1"
+          },
+          {
+            "name": "BM_SUM_SELF_JOIN/1/32768",
+            "value": 1.4001923618001455,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0016641635000000001 s\nthreads: 1"
+          },
+          {
+            "name": "BM_MATRIX_SELF_JOIN/1/32768",
+            "value": 1.7239930303992879,
+            "unit": "s/iter",
+            "extra": "iterations: 10\ncpu: 0.0016796957000000002 s\nthreads: 1"
           }
         ]
       }
